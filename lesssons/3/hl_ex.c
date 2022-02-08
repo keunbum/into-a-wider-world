f7f06c18da5f313ccc4c43495b92668ed4895c7d
keunbum <rkeunbum@gmail.com>
2022-02-09 00:06:57 +0900

    Update ex.c

--- lesssons/3/hl_ex.c
+++ lesssons/3/hl_ex.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
+#define max(x, y) x > y ? x : y
+#define min(x, y) x < y ? x : y
 
 int main() {
-	return !puts("Hello World");
+	int n;
+
+	scanf("%d", &n);
+	const char* S = "Hello World";
+	int len = strlen(S);
+	int cnt = min(n, len);
+	for (int i = 1; i <= cnt; i++) {
+		for (int j = 0; j < i; j++) {
+			putchar(S[j]);
+		}
+		putchar('\n');
+	}
+	return 0;
 }
